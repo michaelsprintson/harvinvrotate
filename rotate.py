aa430fe934bf84b5220c204abbb04da55ae1ede6
Michael Sprintson <michael.sprintson@gmail.com>
2022-06-18 17:24:06 -0400

    added multiple inv rotating feature

--- rotate.py
+++ rotate.py
@@ -12,10 +12,12 @@ import os
 import numpy as np
 from collections import defaultdict
 import discum     
+import sys
 
 # environment variabels
-HARVEY_ID = 251112534231220225
+ID_DICT = {"harvey":251112534231220225, "brittany": 376970858859593728}
 MUDAE_ID = 432610292342587392
+INPUT_NAME = sys.argv[1]
 
 load_dotenv()
 # Grab the API token from the .env file.
@@ -67,41 +69,42 @@ def helloworld(resp):
         # if theres a starter inventory command, do this
         if m['content'][0:9] == "$startinv":
             # grab rotate id and channel to look for
-            id_to_scrape = m['content'][10:]
-            num_of_invs = 0
-            scrape_channel = m['channel_id']
-            if id_to_scrape == "":
-                id_to_scrape = HARVEY_ID
-            
-            # scrape command
-            def startscrape():
+            un_to_scrape = m['content'][10:]
+            if un_to_scrape == INPUT_NAME:
+
+                id_to_scrape = ID_DICT[un_to_scrape]
+                num_of_invs = 0
+                scrape_channel = m['channel_id']
                 
-                print("about to send pr")
-                sleep(1)
-                # send pr to grab bumber of items in inventory
-                bot.sendMessage(m['channel_id'],f"$pr {id_to_scrape}")
-                num_of_invs = inv_size // 40
-                while inv_size == 0:
-                    print("inv size still 0, waiting 5")
-                    sleep(5)
-                    #calculate the number of rotations to do
+                # scrape command
+                def startscrape():
+                    
+                    print("about to send pr")
+                    sleep(1)
+                    # send pr to grab bumber of items in inventory
+                    bot.sendMessage(m['channel_id'],f"$pr {id_to_scrape}")
                     num_of_invs = inv_size // 40
-                print("found", num_of_invs, f"rotations needed for inv size {inv_size}")
-                # perform rotations by posting the inventory by calling mmi at rotation location and letting the command above rotate
-                for i in range(0,num_of_invs+1):
-                    if i != 0:
-                        bot.sendMessage(m['channel_id'],f"$mmi {id_to_scrape} ${i * 40}")
-                    else:
-                        bot.sendMessage(m['channel_id'],f"$mmi {id_to_scrape}")
-                    print("finished rotation, watiing for next")
-                    # wait 5 mins between mmi commands to let mudae timer cool down
-                    sleep(605)
-            while (True):
-                #run the rotator every hour
-                startscrape()
-                print("sleeping 3600")
-                sleep(3600)
-        
+                    while inv_size == 0:
+                        print("inv size still 0, waiting 5")
+                        sleep(5)
+                        #calculate the number of rotations to do
+                        num_of_invs = inv_size // 40
+                    print("found", num_of_invs, f"rotations needed for inv size {inv_size}")
+                    # perform rotations by posting the inventory by calling mmi at rotation location and letting the command above rotate
+                    for i in range(0,num_of_invs+1):
+                        if i != 0:
+                            bot.sendMessage(m['channel_id'],f"$mmi {id_to_scrape} ${i * 40}")
+                        else:
+                            bot.sendMessage(m['channel_id'],f"$mmi {id_to_scrape}")
+                        print("finished rotation, watiing for next")
+                        # wait 5 mins between mmi commands to let mudae timer cool down
+                        sleep(605)
+                while (True):
+                    #run the rotator every hour
+                    startscrape()
+                    print("sleeping 3600")
+                    sleep(3600)
+            
 
             
  
